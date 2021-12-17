90f40cc3e959d09929b0fb59e599bf9a77056c10
slashliu@yunify.com <liuyuqin@liuyuqindeMacBook-Pro.local>
2021-12-17 14:43:02 +0800

    change: 新增对tsx和jsx的支持

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 	};
 
 	const provider = vscode.languages.registerHoverProvider(
-		['scss', 'stylus'],
+		['scss', 'stylus', 'tsx', 'jsx'],
 		{
 			provideHover,
 		},
